321fdbfe818b2fff3e44375b5b348314111ad6f9
CarloBerni <carlo.berni@hetic.net>
2019-03-11 14:29:29 +0100

    beginning JS

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var scene = new Three.scene();
+var camera = new Three.perspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer =new ThreeWebGLRenderer( );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
