6aff0222fc2e8cfb14f89a9e9d96a02110fa190d
CarloBerni <carlo.berni@hetic.net>
2019-03-13 12:25:54 +0100

    last

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-var scene = new Three.scene();
-var camera = new Three.perspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene = new Three.Scene( );
+var camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer =new ThreeWebGLRenderer( );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+window.addEventListener('resize', function(){
+  var Width = window.innerWidth;
+  var Height = window.innerHeight;
+  renderer.setSize(width, height );
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix( );
+});
 
+
+//
 var geometry = new Three.BoxGeometry( 1, 1, 1 );
 
 var material = new Three.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true} );
