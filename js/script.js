46bd57af9003238df166e436661c6737db603e98
CarloBerni <carlo.berni@hetic.net>
2019-03-19 11:48:39 +0100

    last

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 var scene = new THREE.Scene( );
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
 var renderer = new THREE.WebGLRenderer( );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// responsive 
 window.addEventListener( 'resize', function ( ) {
   var width = window.innerWidth;
   var height = window.innerHeight;
@@ -23,7 +24,7 @@ var material = new THREE.MeshBasicMaterial({
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 3;
+camera.position.set( 0, 0, 5 );
 
 
 
