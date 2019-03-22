ba951e7ea85df284e93096b020b25bd4364b0330
CarloBerni <carlo.berni@hetic.net>
2019-03-22 12:13:40 +0100

    search loade

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-var scene = new THREE.Scene( );
-scene.background = new THREE.Color( 0xf0f0f0 );
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-
-var renderer = new THREE.WebGLRenderer( );
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-camera.position.set( 0, 0, 10 );
+var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    camera.position.set(0,0,15);
 
 // light
 
@@ -30,31 +28,58 @@ window.addEventListener( 'resize', function ( ) {
 });
 
 
-//
-var geometry = new THREE.BoxGeometry(1, 1, 2);
+// create earths geometry and material
 
-var material = new THREE.MeshBasicMaterial({
-  color: 0xFF00FF00
+var earthGeometry = new THREE.SphereGeometry( 5, 50, 50);
+var earthMaterial = new THREE.MeshPhongMaterial({
+  color: 0xaaaaaa,
+  specular: 0x333333,
+  shininess: 25
 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
+// create clouds geometry and material
 
+var cloudGeometry = new THREE.SphereGeometry( 5.05, 50, 50);
+var cloudMaterial = new THREE.MeshPhongMaterial({
+  transparent: true,
+  opacity: 0.8
+});
 
-var update = function () {
-  cube.rotation.x += 0.03;
-  cube.rotation.y += 0.005;
+// creat Bumps material and geometry
+
+var bumpGeometry = new THREE.SphereGeometry( 5, 50, 50);
+var bumpMaterial = new THREE.MeshPhongMaterial({
+  transparent: true,
+  opacity: 0.2
+});
+
+// creat stars geometry and material
+
+var starGeometry = new THREE.SphereGeometry( 200, 50, 50);
+var starMaterial = new THREE.MeshPhongMaterial({
+  side: THREE.DoubleSide,
+  shininess: 0
+});
+
+// final
+
+var star = new THREE.Mesh(starGeometry, starMaterial);
+scene.add(star);
+var earth = new THREE.Mesh(earthGeometry, earthMaterial);
+scene.add(earth);
+var clouds = new THREE.Mesh(cloudGeometry, cloudMaterial);
+scene.add(clouds);
+var bump = new THREE.Mesh(bumpGeometry, bumpMaterial);
+scene.add(bump);
+
+var render = function() {
+  earth.rotation.y += .0015;
+  clouds.rotation.y += .0025;
+  clouds.rotation.z += .00125;
+  bump.rotation.y += .0015;
+  renderer.render( scene, camera);
+  requestAnimationFrame(render);
+
 };
+render();
 
-var render = function () {
-  renderer.render(scene, camera);
-};
-
-var GameLoop = function () {
-  requestAnimationFrame(GameLoop);
-
-  update();
-  render();
-};
-
-GameLoop();
